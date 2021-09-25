334ee02e8c89b67dfc6cbe1d1b166ae24fe4c796
Yangying <ryysophie@126.com>
2021-09-25 15:15:29 -0400

    Add expire function

--- WaitForSalary/Assets/Scripts/PostProcessing.cs
+++ WaitForSalary/Assets/Scripts/PostProcessing.cs
@@ -11,8 +11,11 @@ public class PostProcessing : MonoBehaviour
     //public Bloom _Bloom;
     public Text Day;
     public ColorGrading _Color;
-    public int i;
+    public float i;
     public DepthOfField _Depth;
+  
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +24,28 @@ public class PostProcessing : MonoBehaviour
         _Color.saturation.value=0;
         _Color.hueShift.value=0;
         _Depth.focalLength.value=0;
+        i= 10;
         }
 
     // Update is called once per frame
     void Update()
-    {   
-         i =int.Parse(Day.text);
+    {  if(i>=1f){
+         i-=0.5f*Time.deltaTime;}
+       else{
+         i=1f;
+       }
         _Color.saturation.value=Random.Range(-100/i,100/i);
         _Color.hueShift.value=Random.Range(-100/i,100/i);
-        _Depth.focalLength.value=100/i+40;
+        _Depth.focalLength.value=100/i+50;   
 
 
         
+            
     }
+
+    public void Eat(){
+        i+=1;
+    }
+    
+
 }
--- WaitForSalary/Assets/Scripts/TimeCounter.cs
+++ WaitForSalary/Assets/Scripts/TimeCounter.cs
@@ -8,15 +8,17 @@ public class TimeCounter : MonoBehaviour
 
     public Text LeftDay;
     public Image TimeCount;
+    public GameObject[] foods;
+    private int len;
     // Start is called before the first frame update
     void Start()
     {
-        
+         len=foods.Length;  
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         int speed=int.Parse(LeftDay.text);
         if(speed==0){
             Application.LoadLevel(0);
@@ -25,6 +27,9 @@ public class TimeCounter : MonoBehaviour
             TimeCount.fillAmount=1;
             speed-=1;
             LeftDay.text=speed.ToString();
+            int i=Random.Range(0,len);
+            foods[i].GetComponent<Image>().color=new Color(0,0,0,0.5f);
+            foods[i].GetComponent<Button>().enabled=false;
         }
         else{
             TimeCount.fillAmount-=0.1f*speed*Time.deltaTime;
